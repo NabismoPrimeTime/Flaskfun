87eb9cc634687e3de00c45c5d4b87879e317f3ae
supermyle <kylemanley99@gmail.com>
2020-05-29 00:39:13 -0400

    Added PHP, Styling, webscraping in progress

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
 	</p>
 	</div> <!--instructions_container-->
 	<div id="form_container">
-	<form onsubmit="return false">
+	<form action="index.php" method="post"> 
 		<div id="date_form">
 		<label for="date">Date invested in Bitcoin</label>
 		<input id="date_field" name="date" type="date" value="2020-04-01">
@@ -41,9 +41,28 @@
 		<div id="invest_form">
 		<label for="investment">Amount of investestment</label>
 		<span id="currencyinput">$ <input id="invest_field" type="number" name="investment"></span>
-		<input type="submit" value="Submit" onclick="calcROI();">
+		<input type="submit" name="submit" value="Submit" onclick="calcROI();">
 		</div> <!--invest_form-->
 	</form>
+	<?php
+	//Check if the form is submitted
+	if ( isset ( $_POST['submit'] ) ) {
+		//Get Amount of investment from Textfield
+		$invest_amt = $_REQUEST['investment'];
+		echo '<br>Invest_amt = $' . $invest_amt . '<br>';
+		//Get Date invested from Textfield and convert it to alphabetical for use with scraper
+		$timestamp = strtotime($_POST['date']); //Get UNIX Timestamp to be parsed to day/month/year
+		$day=date('d',$timestamp); //Get Numeric Day
+		$month=date('M',$timestamp); //Get Alphabetic Month
+		$year=date('Y',$timestamp); //Get Numeric Year
+		echo 'month = ' . $month . ' day = ' . $day . ' year = ' . $year . '<br>';
+		
+		//Link to scraper library
+		include('simple_html_dom.php');
+		$html =	file_get_html('https://coinmarketcap.com/currencies/bitcoin/historical-data/?start=20130501&end=20200528');
+		echo $html->find('td[class="cmc-table__cell cmc-table__cell--sticky cmc-table__cell--left"]',0)->plaintext; //Create table from coinmarketcap.com
+	}
+	?>
 	</div> <!--form_container-->
 	</div> <!--content-->
 	</div> <!--content_container-->
